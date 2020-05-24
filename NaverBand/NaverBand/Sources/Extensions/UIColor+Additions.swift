776ef9b96019393ceb221a134d1e8509b2cce24c
juyeonblue <jyp0211@ajou.ac.kr>
2020-05-24 22:15:27 +0900

    Connect storyboards

---
+++ NaverBand/NaverBand/Sources/Extensions/UIColor+Additions.swift
@@ -0,0 +1,29 @@
+//
+//  UIColor+Additions.swift
+//  양종훈,변정인_네이버 밴드
+//
+//  Generated on Zeplin. (2020. 5. 24.).
+//  Copyright (c) 2020 __MyCompanyName__. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var white: UIColor {
+    return UIColor(white: 245.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var naver: UIColor {
+    return UIColor(red: 25.0 / 255.0, green: 206.0 / 255.0, blue: 97.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var maintext: UIColor {
+    return UIColor(white: 49.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var subtext: UIColor {
+    return UIColor(white: 162.0 / 255.0, alpha: 1.0)
+  }
+
+}
---
+++ NaverBand/TabBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarViewController.swift
+//  NaverBand
+//
+//  Created by 박주연 on 2020/05/24.
+//  Copyright © 2020 박주연. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+}
+}
